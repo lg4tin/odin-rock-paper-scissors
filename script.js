b459c588aace34aa34da54cd2ccd231cf6773901
lg4tin <lg4tin@gmail.com>
2023-08-19 13:15:58 -0400

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+
+    if (randomNum === 0) {
+        return 'rock';
+    } else if (randomNum === 1) {
+        return 'scissors';
+    } else {
+        return 'paper';
+    }
+}
